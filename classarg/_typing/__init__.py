04f77618806fcc3c7d37498ef6cbe88e744d0f04
Ian Chen <r05921035@ntu.edu.tw>
2018-06-24 04:46:34 +0800

    Try to merge coveralls results in different python versions

--- classarg/_typing/__init__.py
+++ classarg/_typing/__init__.py
@@ -1,6 +1,6 @@
 from ..utils import compatible_with
 
-from .py34 import get_type_hints, Union, Optional
+# from .py34 import get_type_hints, Union, Optional
 
 __all__ = (
     'get_type_hints',
@@ -8,8 +8,8 @@ __all__ = (
     'Optional',
 )
 
-# if compatible_with(3, 5):
-#     from typing import get_type_hints, Union, Optional
+if compatible_with(3, 5):
+    from typing import get_type_hints, Union, Optional
 
-# else:
-#     from .py34 import get_type_hints, Union, Optional
+else:
+    from .py34 import get_type_hints, Union, Optional
