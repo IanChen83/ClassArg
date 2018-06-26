06c1fe0fc4015a1e72f18cce0d762179d552a8fc
Ian Chen <r05921035@ntu.edu.tw>
2018-06-26 17:55:10 +0800

    Add python3.3 CI; try to fix previous bugs

--- classarg/_typing.py
+++ classarg/_typing.py
@@ -1,9 +1,10 @@
 # Some code are modified from cpython's typing module.
 from inspect import getfullargspec
 
-from ..utils import compatible_with
+from .utils import compatible_with
 
 __all__ = (
+    'normalize_type',
     'get_type_hints',
     'Union',
     'Optional',
@@ -24,35 +25,48 @@ _supported_names = {
     'None': None,
     'NoneType': NoneType,
 }
+_aux_mapping = {}
 
 
 def _eval_type_from_str(obj):
     try:
         code = compile(obj, '<string>', 'eval')
-        return eval(code, dict(__builtin__=None, **_supported_names))
+        ret = eval(code, dict(__builtin__=None,
+                              **_supported_names))
+        return normalize_type(ret)
+
     except (SyntaxError, NameError):
         raise TypeError('Failed to parse type from string')
 
 
-def _type_transform(obj):
+def _normalize_meta_type(obj):
+    if hasattr(obj, '__origin__'):  # must be instance of meta types
+        if obj.__origin__ in _aux_mapping:
+            # Create a new obj because it's not good to modify obj.__origin__
+            # directly.
+            origin, args = _aux_mapping[obj.__origin__], obj.__args__
+            obj = origin[args]
+            obj.__origin__ = origin
+
+        if obj.__origin__ in _meta:
+            obj.__args__ = tuple(normalize_type(arg)
+                                 for arg in obj.__args__)
+            return obj
+
+    raise TypeError('Failed to normalize {}'.format(obj))
+
+
+def normalize_type(obj):
     if obj is None:
         return NoneType
 
     elif obj in _builtin:
         return obj
 
-    if isinstance(obj, str):
-        t = _eval_type_from_str(obj)
-        if t in _builtin:
-            return t
-        elif hasattr(t, '__origin__') and t.__origin__ in _meta:
-            return t
-    else:
-        t = type(obj)
-        if t in _meta:
-            return t
+    elif isinstance(obj, str):
+        return _eval_type_from_str(obj)
 
-    raise TypeError('Type {} is not supported'.format(t))
+    return _normalize_meta_type(obj)
 
 
 def get_type_hints(obj, globalns=None, localns=None):
@@ -60,12 +74,12 @@ def get_type_hints(obj, globalns=None, localns=None):
     annotations = spec.annotations
     spec_defaults = spec.defaults or tuple()
     spec_kwonlydefaults = spec.kwonlydefaults or {}
-    defaults = dict(**spec_kwonlydefaults,
+    defaults = dict(spec_kwonlydefaults,
                     **{k: v for k, v in zip(spec.args, spec_defaults)})
 
     ret = {}
     for key, value in annotations.items():
-        ret[key] = _type_transform(value)
+        ret[key] = normalize_type(value)
 
         if key in defaults and defaults[key] is None:
             ret[key] = Optional[ret[key]]
@@ -128,16 +142,15 @@ else:
 
         def __getitem__(cls, params):
             if not isinstance(params, tuple):
-                params = (params, )
-
-            return cls(params)
-
-        def __call__(cls, params):
-            """Ensure that all inputs are supported types"""
-            params = tuple(_type_transform(p) for p in params)
+                params = (normalize_type(params), )
+            else:
+                params = tuple(normalize_type(p) for p in params)
 
             ret = super().__call__()
             return ret.__transform__(params)
+
+        def __call__(cls, params):
+            raise TypeError('Cannot directly intialize special typing classes')
 
         def __repr__(cls):
             return 'typing.' + cls._name
@@ -193,3 +206,6 @@ else:
 
     class Set(_SingleCollection):
         pass
+
+_aux_mapping[list] = List
+_aux_mapping[set] = Set
--- tests/test_typing.py
+++ tests/test_typing.py
@@ -1,5 +1,5 @@
 import pytest
-from classarg._typing import Union, Optional, List, Set
+from classarg._typing import Union, Optional, List, Set, normalize_type
 
 
 NoneType = type(None)
@@ -32,11 +32,42 @@ def test_parse_metaclass(type1, type2, expect):
         assert isinstance(expect, e_info.type)
 
     else:
+        res = normalize_type(type1[type2])
         if expect in _builtin:
-            assert type1[type2] is expect
+            assert res is expect
 
         else:
-            res = type1[type2]
+            type_name, types = expect
+
+            assert (str(res.__origin__), res.__args__) == (type_name, types)
+
+
+def _gen_type_str_testcase():
+    yield 'Union[int]', int
+    yield 'Union[None]', NoneType
+    yield 'Union[Union[None]]', NoneType
+    yield 'Union[int, str]', ('typing.Union', (int, str))
+    yield 'Optional[None]', NoneType
+    yield 'Optional[int]', ('typing.Union', (int, NoneType))
+    yield 'List[int]', ('typing.List', (int, ))
+    yield 'Set[int]', ('typing.Set', (int, ))
+
+
+@pytest.mark.parametrize('type_str, expect', list(_gen_type_str_testcase()))
+def test_parse_type_str(type_str, expect):
+    if isinstance(expect, Exception):
+        with pytest.raises(Exception) as e_info:
+            ret = normalize_type(type_str)
+            print(ret)  # only print if not raising error
+
+        assert isinstance(expect, e_info.type)
+
+    else:
+        if expect in _builtin:
+            assert normalize_type(type_str) is expect
+
+        else:
+            res = normalize_type(type_str)
             type_name, types = expect
 
             assert (str(res.__origin__), res.__args__) == (type_name, types)
